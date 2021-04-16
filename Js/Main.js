11daade17be5c321dcd96a88dc335483670bd51f
Darleysantana <darleyhyuga@gmail.com>
2021-04-15 23:10:30 -0300

    Entrega

---
+++ Js/Main.js
@@ -0,0 +1,26 @@
+/*
+var pagina = 1;
+var produto = 'https://frontend-intern-challenge-api.iurykrieger.now.sh/products?page='
+const Button = getElement('.button'),
+      container = getElement('.imagem'),
+      erroMessage = getElement('.error');
+
+var nome, // Nome ou numero passado na caixa de busca
+    imagem, // Responsavel por guardar os dados recebidos da API
+    descricao; // Responsavel por receber o HTML
+
+    function requestInfo(url, name) {
+        fetch(url + name)
+          .then(response => response.json())
+          .then(data => {
+            imagem = data;
+          })
+          .catch(err => console.log(err));
+      }
+      requestInfo(produto, nome);
+      
+      searchButton.addEventListener('click', event => {
+       produto = produto + pagina;
+});
+      */
+     
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
+### Teste-DSN
+    Tudo que está neste arquivo foi o que consegui fazer com as pesquisas e o tempo dado, pois não entendo de API's
+## Completos:
+>>> HTML,
+>>> CSS 70%,
 
+## Erros:
+>>> Parte do desing linear-gradient pois nunca tinha visto,
+>>> Responsividade não lembrei como deixa-lo funcional,
+>>> Incremento da API.
